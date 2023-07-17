ca6c59601d4e0104a79df92f338ea3d2e139eaa5
perintyler <perintyler@gmail.com>
2023-07-17 14:02:01 -0400

    barebones guitar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import './App.css';
+import { useEffect } from 'react';
+import { Fretboard } from '@moonwave99/fretboard.js';
+
+
+function Guitar() {
+  var fretboard = null;
+
+  useEffect(() => {
+
+      if (fretboard === null) {
+        fretboard = new Fretboard({
+          el: '#fretboard',
+          fretColor: 'blue',
+          dotFill: 'red'
+        });
+      }
+
+      if (!fretboard.baseRendered) {
+        fretboard.renderScale({type:"chromatic"});
+        fretboard.style({text: ({ note }) => note}); // show note names
+      }
+
+  });
+
+  return <figure id="fretboard"></figure>;
+}
+
 
 function App() {
+
   return (
     <div className="App">
+      <Guitar></Guitar>
     </div>
   );
+
 }
 
 export default App;
