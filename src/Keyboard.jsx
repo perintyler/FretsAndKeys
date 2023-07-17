36ff62606ed17f7436405de4fbfa404db6ce778c
perintyler <perintyler@gmail.com>
2023-07-17 16:44:09 -0400

    create a NoteLabel component for pitch and octave text

--- src/Keyboard.jsx
+++ src/Keyboard.jsx
@@ -1,1 +1,16 @@
 /* Keyboard.jsx */
+
+import 'react-piano/dist/styles.css';
+
+import { Piano, MidiNumbers } from 'react-piano';
+import { getPitchName, getOctaveNumber, isBlackKey } from './notes_api';
+
+function NoteLabel({ midiNumber, color }) 
+{
+  return (
+    <div style={{"color": color}} className="note-label-container unselectable-text">
+      <span className="note-label-name">{getPitchName(midiNumber)}</span> 
+      <span className="note-label-octave"> {getOctaveNumber(midiNumber)} </span>
+    </div>
+  );
+}
