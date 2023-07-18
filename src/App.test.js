6b90d671075810a9cfefad7071ad2a7d869bbad8
perintyler <perintyler@gmail.com>
2023-07-18 12:16:57 -0400

    test note labels

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('piano note labels are rendered', () => {
+  render(<App />);
+
+  let noteLabels = screen.getAllByTestId('note-label');
+  expect(noteLabels).toHaveLength(88);
+});
+
