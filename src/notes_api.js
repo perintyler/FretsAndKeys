e43b97eb923ab94552696a03fc1ff955116dbf7e
perintyler <perintyler@gmail.com>
2023-07-18 12:16:34 -0400

    fix getMidiNumber

--- src/notes_api.js
+++ src/notes_api.js
@@ -1,8 +1,12 @@
 /* notes_api.js */
 
-const FIRST_MIDI_NUMBER = -21;
+const FIRST_MIDI_NUMBER = -12; // C0
 
-const PITCH_NAMES = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+const OCTAVE_SIZE = 12;
+
+const PITCH_NAMES = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
+
+const ALTERNATE_PITCH_NAMES = ["c", "db", "d", "eb", "e", "f", "gb", "g", "ab", "a", "bb", "b"];
 
 const BLACK_NOTES = ["A#", "C#", "D#", "F#", "G#"];
 
@@ -14,12 +18,12 @@ const ALL_NOTE_NAMES = new Array(8).fill(PITCH_NAMES).map( // stops after the 8t
 
 export function getPitchName(midiNumber) 
 {
-  return PITCH_NAMES[(midiNumber+FIRST_MIDI_NUMBER) % 12];
+  return PITCH_NAMES[(midiNumber+FIRST_MIDI_NUMBER) % OCTAVE_SIZE];
 } // -> str
 
 export function getOctaveNumber(midiNumber)
 {
-  return Math.floor((midiNumber+FIRST_MIDI_NUMBER) / 12);
+  return Math.floor((midiNumber+FIRST_MIDI_NUMBER) / OCTAVE_SIZE);
 } // -> int
 
 export function getNoteAsText(midiNumber, includeOctave = true) 
@@ -40,7 +44,19 @@ export function isBlackKey(midiNumber)
 
 export function getMidiNumber(noteName, octaveNumber)
 {
-  return PITCH_NAMES.indexOf(noteName) + octaveNumber*PITCH_NAMES.length - FIRST_MIDI_NUMBER;
+  noteName = noteName.toLowerCase();
+
+  var pitchIndex;
+  if (PITCH_NAMES.includes(noteName)) {
+    pitchIndex = PITCH_NAMES.indexOf(noteName);
+  } else if (ALTERNATE_PITCH_NAMES.includes(noteName)) {
+    pitchIndex = ALTERNATE_PITCH_NAMES.indexOf(noteName);
+  } else {
+    pitchIndex = 0;
+    console.error('(getMidiNumber) invalid note name: ' + noteName);
+  }
+
+  return pitchIndex + octaveNumber*OCTAVE_SIZE + FIRST_MIDI_NUMBER;
 } // -> int
 
 export function getMidiNumberFromNoteName(noteName)
@@ -48,3 +64,11 @@ export function getMidiNumberFromNoteName(noteName)
   return ALL_NOTE_NAMES.indexOf(noteName) + FIRST_MIDI_NUMBER;
 } // -> int
 
+export function getFretAndString(midiNumber)
+{
+  let fret = (midiNumber - MIDI_NUMBER_OF_FIRST_FRET) % 16;
+  let string = -((midiNumber - MIDI_NUMBER_OF_FIRST_FRET - fret)/5 - 6)
+  console.log(midiNumber, fret, string);
+
+  return {'fret': fret, 'string': string};
+}
