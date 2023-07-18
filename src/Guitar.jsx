d0f611add573ab4699c66b38fe649abcaa3d6b4d
perintyler <perintyler@gmail.com>
2023-07-18 16:27:48 -0400

    guitar component note selection/de-selection

--- src/Guitar.jsx
+++ src/Guitar.jsx
@@ -1,28 +1,68 @@
 /* Guitar.jsx */
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Fretboard } from '@moonwave99/fretboard.js';
+import { getMidiNumber, getMidiNumberFromFretAndString, getNoteAsText } from './notes_api'
 
-function createFretboard() {
-  const fretboard = new Fretboard({
+function createFretboard() 
+{
+  return new Fretboard({
     el: '#fretboard',
-    fretColor: 'blue',
-    dotFill: 'red'
-  });
+    fretColor: 'black',
+    dotFill: 'white',
+    dotText: ({ note }) => note,
+    dotStrokeColor: ({ moving }) => 'black',
+  }).render();
 
-  fretboard.renderScale({type:"chromatic"}); // render all notes
-
-  fretboard.style({text: ({ note, octave }) => `${note}${octave}`}); // fretboard.js api doesn't support rendering react components in the 'get text' callback
-
-  return fretboard;
+  // fretboard.renderScale({type:"chromatic"}); // render all notes
+  // fretboard.style({text: ({ note, octave }) => `${note}${octave}`}); // fretboard.js api doesn't support rendering react components in the 'get text' callback
+  // fretboard.style({text: ({ note, octave }) => getMidiNumber(note, octave)}); // fretboard.js api doesn't support rendering react components in the 'get text' callback
 }
 
-export default function Guitar() {
+function createFretboardJSDotObject(fret, string) 
+{
+  let midiNumber = getMidiNumberFromFretAndString(fret, string);
+  let noteAsText = getNoteAsText(midiNumber);
+
+  return {
+    fret: fret,
+    string: string,
+    note: noteAsText,
+  };
+}
+
+export default function Guitar() 
+{
   var fretboardRef = useRef(null);
+  var [selected, setSelectedNotes] = useState([]);
+
+  function updateSelectedNotes(fret, string) 
+  {
+    var numNotesBeforeNewSelection = selected.length;
+
+    selected = selected.filter(selected => fret !== selected.fret || string !== selected.string);
+    if (selected.length === numNotesBeforeNewSelection) {
+      selected.push({fret: fret, string: string});
+    }
+
+    const dots = selected.map((note) => createFretboardJSDotObject(note.fret, note.string));
+    fretboardRef.current.setDots(dots).render();
+
+    setSelectedNotes(selected);
+  }
 
   useEffect(() => {
-      if (fretboardRef.current === null) {
-        fretboardRef.current = createFretboard();
+      if (fretboardRef.current === null) 
+      {
+        fretboardRef.current = new Fretboard({
+          el: '#fretboard',
+          fretColor: 'black',
+          dotFill: 'white',
+          dotText: ({ note }) => note,
+          dotStrokeColor: ({ moving }) => 'black',
+        }).render();
+
+        fretboardRef.current.on('click', ({ fret, string }) => updateSelectedNotes(fret, string));
       }
   });
 
