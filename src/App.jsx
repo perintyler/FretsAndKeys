5b95f513bdb660890fb63b47da6318171e9a90c6
perintyler <perintyler@gmail.com>
2023-07-19 13:29:53 -0400

    simple header bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css';
 import Guitar from './Guitar';
 import Keyboard from './Keyboard';
 import { useEffect, useRef, useState } from 'react';
+import {ReactComponent as GithubLogo} from './github-logo.svg';
+
 
 function Instruments()
 {
@@ -44,9 +46,23 @@ function Instruments()
   );
 }
 
+function HeaderBar()
+{
+  return (
+    <div id="header">
+      <ul>
+        <li><h1>FretsAndKeys.com</h1></li>
+        <li><GithubLogo style={{textColor: 'black'}} /></li>
+      </ul>
+    </div>
+
+  )
+}
+
 function App() {
   return (
     <div id="App">
+      <HeaderBar />
       <Instruments></Instruments>
     </div>
   );
