7a02a8f087153ffdda3ae71bc5d9c9081a0bea7f
perintyler <perintyler@gmail.com>
2023-07-21 13:36:09 -0400

    header bar help modal

--- src/HeaderBar.jsx
+++ src/HeaderBar.jsx
@@ -3,25 +3,59 @@
 import { ReactComponent as GithubLogo } from './svg/github-logo.svg';
 import { ReactComponent as HelpIcon } from './svg/question-mark-icon.svg';
 import { ReactComponent as TitleIcon } from './svg/frets-and-keys.svg';
+import { useState } from 'react';
+
+import CloseButton from 'react-bootstrap/CloseButton';
+import Modal from 'react-modal';
 
 export default function HeaderBar()
 {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  let helpIcon = <button><HelpIcon style={{width: "45px", height: "45px", paddingRight: "10px"}}/></button>;
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  var helpMessageModal = (
+    <Modal
+      isOpen={modalIsOpen}
+      onAfterOpen={afterOpenModal}
+      onRequestClose={closeModal}
+      contentLabel="Example Modal"
+    >
+      <CloseButton onClick={closeModal} id="help-message-box-close-button" aria-label="Hide" />
+    </Modal>
+  );
+
   return (
     <div id="header">
-      <ul style={{verticalAlign: "center"}}>
-        <li style={{marginLeft: "12px", paddingTop: "12px"}}>
+      <ul>
+        <li style={{paddingTop: "0px"}}>
           <a href="http://github.com/perintyler/FretsAndKeys">
-            <GithubLogo style={{width: "20px"}} />
+            <GithubLogo style={{width: "18px"}} />
           </a>
         </li>
-        <li style={{marginLeft: "16px"}}>          
+        <li>          
           <TitleIcon style={{paddingTop: "7px", height: "38px"}}/>
         </li>
-        <li style={{float: "right", paddingTop: "6px"}}>
-          <HelpIcon style={{width: "45px", height: "45px", paddingRight: "10px"}}/>
-          
+        <li>
+            <div onClick={()=>setModalIsOpen(true)}>
+              <HelpIcon style={{width: "20px", paddingTop: "9px"}}/>
+            </div>
         </li>
       </ul>
+      {helpMessageModal}
     </div>
   )
 }
