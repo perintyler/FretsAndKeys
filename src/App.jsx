f2ec08294396c991da62c9e32eaa63c8fd183057
perintyler <perintyler@gmail.com>
2023-07-21 13:48:48 -0400

    remove redundant instruments component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import Guitar from './Guitar';
 import Keyboard from './Keyboard';
@@ -12,8 +12,8 @@ import MuteSwitch from './MuteSwitch';
 import ScaleSelection from './ScaleSelection';
 import DetectedChordsList from './DetectedChordsList';
 
-import { getNoteAsText, getMidiNumber, getPitchName } from './notes_api';
-import { Chord, Scale } from "tonal";
+import { getNoteAsText, getMidiNumber } from './notes_api';
+import { Scale } from "tonal";
 
 import Button from 'react-bootstrap/Button';
 
@@ -21,44 +21,53 @@ import * as Tone from 'tone'; // can i use modern import for this
 
 const synth = new Tone.Synth().toDestination();
 
+const LAST_NOTE = getMidiNumber("G#", 5); // last fret on the fretboard
+
+function ExplanationBox() {
+  return (
+    <div id="explanation-banner">
+      Click on a fret or a key and the corresponding note will show up
+      on the other instrument
+    </div>
+  );
+}
+
 function ClearNotesButton({ onClick })
 {
   return (
-    <div className="full-width centered-text">
       <Button 
         onClick={()=>onClick()} 
         className="vertical-center" 
         id="clear-notes-button"
         children="Clear Notes"
       />
-    </div>
   );
 }
 
-function Instruments()
+export default function App()
 {
   var [selectedNotes, setSelectedNotes] = useState([]);
   var [isMuted, setIsMuted] = useState(false);
 
-  function updateSelectedNotes(midiNumber) {
-    var filteredNotes = selectedNotes.filter((selected) => midiNumber !== selected);
-    if (selectedNotes.length !== filteredNotes.length) { 
-      // deselect a previously selected note
-      setSelectedNotes(filteredNotes);
-    } else { 
-      // select a new note
-      if (!isMuted) {
-        synth.triggerAttackRelease(getNoteAsText(midiNumber), "8n");
+  function updateSelectedNotes(midiNumber) 
+  {
+    if (midiNumber <= LAST_NOTE) {
+      var filteredNotes = selectedNotes.filter((selected) => midiNumber !== selected);
+      if (selectedNotes.length !== filteredNotes.length) { // deselect a previously selected note
+        setSelectedNotes(filteredNotes);
+      } else { // select a new note
+        if (!isMuted) {
+          synth.triggerAttackRelease(getNoteAsText(midiNumber), "8n");
+        }
+        setSelectedNotes(selectedNotes.concat([midiNumber]));
       }
-      setSelectedNotes(selectedNotes.concat([midiNumber]));
     }
   }
 
-  function showScale(rootNote, scale) {
-    console.log(rootNote, scale)
+  function showScale(rootNote, scale) 
+  {
     let pitches = Scale.get(`${rootNote.toLowerCase()} ${scale}`)['notes'];
     let midiNumbers = [2,3,4,5,6].map(octave => pitches.map(pitch => getMidiNumber(pitch, octave))).flat(1);
-    console.log(midiNumbers);
     setSelectedNotes(midiNumbers);
   }
 
@@ -76,37 +85,24 @@ function Instruments()
     />
   );
 
-  var clearButton = <ClearNotesButton onClick={()=>setSelectedNotes([])} />;
-
-  var muteButton = <MuteSwitch onChange={() => setIsMuted(!isMuted)} />
-
-  var scaleSelection = <ScaleSelection showScale={showScale} />;
-
-  var chordList = <DetectedChordsList notes={selectedNotes} />;
-
   return (
-    <div id="instruments-container">
-      <MuteSwitch onChange={() => setIsMuted(!isMuted)} />
-      <div id="guitar-container">{guitar}</div>
-      <div id="keyboard-container">{keyboard}</div>
-      <div id="bottom-section">
-        <div className="horizontal-row standard-vertical-margin">
-          <ScaleSelection showScale={showScale} />
-          <DetectedChordsList notes={selectedNotes} />
+    <div id="App">
+      <HeaderBar />
+      <div id="instruments-container">
+        <ExplanationBox />
+        <MuteSwitch onChange={() => setIsMuted(!isMuted)} />
+        <div id="guitar-container">{guitar}</div>
+        <div id="keyboard-container">{keyboard}</div>
+        <div id="bottom-section">
+            <div style={{display:"inline-block"}}>
+              <ScaleSelection showScale={showScale}/>
+            </div>
+            <div id="detected-chords-card-container">
+              <DetectedChordsList notes={selectedNotes}/>
+            </div>
+          <ClearNotesButton onClick={()=>setSelectedNotes([])} />
         </div>
-        <ClearNotesButton onClick={()=>setSelectedNotes([])} />
       </div>
     </div>
   );
 }
-
-function App() {
-  return (
-    <div id="App">
-      <HeaderBar />
-      <Instruments></Instruments>
-    </div>
-  );
-}
-
-export default App;
