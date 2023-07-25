9f77e3f2410791856c930cc4f403d0d86d08510b
perintyler <perintyler@gmail.com>
2023-07-25 19:14:30 -0400

    use bootstrap lists for detected chords

--- src/DetectedChordsCard.jsx
+++ src/DetectedChordsCard.jsx
@@ -6,6 +6,9 @@ import { getPitchName } from './notes_api'
 
 import Card from 'react-bootstrap/Card';
 
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+
 export default function DetectedChordsCard({ notes })
 {
   let chords = Chord.detect(notes.map(getPitchName));
@@ -13,15 +16,14 @@ export default function DetectedChordsCard({ notes })
   var cardBody;
   if (chords.length === 0) {
     cardBody = (
-      <div className="centered-text" style={{ paddingTop: "36px" }}>
+      <div className="centered-text vertically-centered-text">
         <h5>No chords detected</h5>
       </div>
     );
   } else {
-    let chordListItems = chords.map((chord,i) => <li key={i}>{chord}</li>);
-    cardBody = <ul>{ chordListItems }</ul>;
+    let chordListItems = chords.map((chord,i) => <ListGroupItem key={i}>{chord}</ListGroupItem>);
+    cardBody = <ListGroup>{ chordListItems }</ListGroup>;
   }
-
 
   return (
     <Card id="detected-chords-card">
