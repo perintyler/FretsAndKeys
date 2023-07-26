1ec1a2637b7f0e15add949a9178bb022b7298a74
perintyler <perintyler@gmail.com>
2023-07-26 12:26:46 -0400

    remove header bar help icon for now

--- src/HeaderBar.jsx
+++ src/HeaderBar.jsx
@@ -1,27 +1,14 @@
 /* HeaderBar.jsx */
 
 import { ReactComponent as GithubLogo } from './svg/github-logo.svg';
-import { ReactComponent as HelpIcon } from './svg/question-mark-icon.svg';
 import { ReactComponent as TitleIcon } from './svg/frets-and-keys.svg';
-import { useState } from 'react';
-
-import CloseButton from 'react-bootstrap/CloseButton';
-import Modal from 'react-modal';
-import ReactPlayer from 'react-player'
 
 export default function HeaderBar()
 {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  function closeModal() 
-  {
-    setModalIsOpen(false);
-  }
-
   return (
     <div id="header">
       <ul>
-        <li style={{paddingTop: "0px"}}>
+        <li>
           <a href="http://github.com/perintyler/FretsAndKeys">
             <GithubLogo style={{width: "18px"}} />
           </a>
@@ -29,34 +16,8 @@ export default function HeaderBar()
         <li>          
           <TitleIcon style={{paddingTop: "7px", height: "38px"}}/>
         </li>
-        <li>
-            <div onClick={()=>setModalIsOpen(true)}>
-              <HelpIcon style={{width: "20px", paddingTop: "9px"}}/>
-            </div>
-        </li>
+        <li></li>
       </ul>
-
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={()=>{}}
-        onRequestClose={closeModal}
-        contentLabel="Example Modal"
-        appElement={document.getElementById("App")}
-        style={{ content: {backgroundColor: "var(--night)"}}}
-      >
-        <>
-          <CloseButton style={{backgroundColor: "white", height: "30px", width: "30px"}} onClick={closeModal} id="help-message-box-close-button" aria-label="Hide" />
-          <div style={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <ReactPlayer
-              style={{ border: "2px solid", borderColor: "var(--emerald)" }}
-              playing={modalIsOpen}
-              url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" 
-            />
-          </div>
-        </>
-      </Modal>
-
-
     </div>
   )
 }
