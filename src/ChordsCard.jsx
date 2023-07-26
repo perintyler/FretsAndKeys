615b9836174b17f38835d787d849220fdcbf8829
perintyler <perintyler@gmail.com>
2023-07-26 13:10:58 -0400

    ChordListItem component

--- src/ChordsCard.jsx
+++ src/ChordsCard.jsx
@@ -9,6 +9,41 @@ import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
+function ChordListItem({ chord })
+{
+  chord = chord.replace('M', 'major')
+               .replace(/[m,](?=[^maj|major])/, 'minor'); // replace 'm' with 'minor' unless the 
+                                                          // 'm' is part of 'maj' or 'major'
+  var rootNote;
+  var chordInfo;
+  if (chord.charAt(1) === "#") { // root note is sharp
+    rootNote = chord.slice(0, 2);
+    rootNote = rootNote.replace('#', '♯');
+    chordInfo = chord.slice(2);
+  } else {
+    rootNote = chord.slice(0, 1);
+    chordInfo = chord.slice(1);
+  }
+
+  chordInfo = chordInfo.replace('b', '♭').replace('#', '♯');
+
+  var bassNote;
+  if (chordInfo.includes('/')) {
+    bassNote = chordInfo.split('/')[1];
+    chordInfo = chordInfo.split('/')[0];
+  } else {
+    bassNote = null;
+  }
+
+  return (
+    <ListGroupItem className="chord-list-item" key={chord}>
+      <span className="chord-root-note">{rootNote}</span>
+      <span className="chord-info">{chordInfo}</span>
+      <span className="chord-bass-note">{bassNote ? `/ ${bassNote}` : ""}</span>
+    </ListGroupItem>
+  );
+}
+
 export default function ChordsCard({ notes })
 {
   let chords = Chord.detect(notes.map(getPitchName));
@@ -21,7 +56,7 @@ export default function ChordsCard({ notes })
       </div>
     );
   } else {
-    let chordListItems = chords.map((chord,i) => <ListGroupItem key={i}>{chord}</ListGroupItem>);
+    let chordListItems = chords.map(chord => <ChordListItem chord={chord} />);
     cardBody = <ListGroup>{ chordListItems }</ListGroup>;
   }
 
